45d3d02cd8d59a0d4e74d819cba3d82c2aa93457
Sanskruti Jaiswal <srjbeliever0129@gmail.com>
2024-02-02 08:41:34 +0530

    server2.js

---
+++ server2.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const admin = require('firebase-admin');
+const cors = require('cors');
+
+
+const app = express();
+const port =  process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors());
+
+
+
+
+
+const serviceAccount = require('./path to firebase account.json');
+admin.initiliazeApp({
+    credential : admin.credential.cert(serviceAccount),
+    // databaseURL : 'https://fir-auth-1-1e3e7.firebaseio.com'
+    });
+
+mongoose.connect('mongodb://localhost:27017/movies', 
+        {useNewUrlParser: true, useUnifiedTopology: true})
+
+const db = mongoose.connection ; 
+
+db.on('error' , console.error.bind(console, "Connection error"));
+
+db.once ('open',()=>{
+    console.log('Database connected');
+});
+
+
+// user_movie_ratings 
+
+const Rating = mongoose.model('User_Rating', ratingSchema);
+
+// define the endpoint for user movie ratings 
+
+app.post('/api/rating' , async (req , res)=>{
+    try{
+        const {userId , movieId , rating} = req.body;
+        const userRating = new Rating({movieId , rating});
+        await userRating.save();
+        res.json(userRating);
+        res.status(201).json({message : "Rating added successfully"});
+    }catch(err){ 
+        console.error('Error saving rating :' , err);
+        res.status(500).json({error : 'Internal Error: saving rating'});
+    }
+});
+
+
+
+
+
